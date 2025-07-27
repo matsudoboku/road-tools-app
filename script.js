017c6305dd250776112bbde7fd76af139e3bb8cf
matsudoboku <muramatsu.matsukawa@gmail.com>
2025-07-27 21:57:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const LS_KEY = 'paveAppAllSites_v5'; // 新バージョンに合わせて更新
 let allSites = {};
 let currentSite = '';
-let currentSite = '';
 const paveTypes = ["アスファルト", "コンクリート", "オーバーレイ"];
 const worksList = [
   { id: "Works", label: "工種設定", always: true, panel: "panelWorks" },
